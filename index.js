8ee3caa96718a70e5d68e0157131410f5f68dfc3
Ian Van Winkle <ianxvanwinkle@gmail.com>
2024-10-21 22:22:54 -0400

    fifth commit

--- index.js
+++ index.js
@@ -1,118 +1,152 @@
 document.addEventListener("DOMContentLoaded", (evt) => {
-const form = document.getElementById("form")
+  const form = document.getElementById("form")
 
-const userName = document.getElementById("name")
-userName.addEventListener("keypress", checkName, false)
+  const userName = document.getElementById("name")
+  userName.addEventListener("keypress", checkName, false)
+  // userName.setAttribute("Enter Name")
 
-const continents = document.querySelector("#continent-select")
-continents.addEventListener("click", (a) => {
-  console.log("Selected Continent:", a.target.value)
-})
-// On my third commit I had this working ^^^^. It would show in the
-// console when a continent was slected but has since stopped working.
+  const continents = document.querySelector("#continent-select")
+  continents.addEventListener("click", (a) => {
+    console.log("Selected Continent:", a.target.value)
+  })
+  // On my third commit I had this working ^^^^. It would show in the
+  // console when a continent was slected but has since stopped working.
 
+  const title = createTitle()
+  const btn = createBtn()
+  const name = document.getElementById("name")
+  form.appendChild(title)
+  form.appendChild(btn)
+  // form.appendChild(createWarningBox())
+  form.addEventListener("submit", validate)
+  form.addEventListener("click", displayResult)
 
-// continents.onclick = () => {
-//   console.log("Event handler")
-// }
+  // submitBtn.classList.add("active");
+  // submitBtn.style.backgroundColor = "#8e44ad";
 
-// form.style.border = "1px solid black"
-// form.style.padding = "10px"
-// Was trying to edit the page with both CSS and JS. Ran into issues with having both.
+  // ----------------Functions----------------------------
 
-const title = createTitle()
-const btn = createBtn()
-form.appendChild(title)
-form.appendChild(btn)
-form.appendChild(createWarningBox())
+  function createTitle() {
+    const div = document.createElement("div")
+    div.innerHTML = "<strong>Sign up for your destination today!</strong>"
 
-
-
-// function createTitle() {
-  const div = document.createElement("div")
-  div.innerHTML = "<strong>Sign up for your destination today!</strong>"
-
-//   div.classList.add("title")
-//   return div
-// }
-
-// Wasn't sure if I needed these as functions cause I think they worked with or without.
-
-function createBtn() {
-  const btn = document.createElement("button")
-  btn.textContent = "Click here"
-  // btn.addEventListener("click", signUp)
-  // btn.classList.add("btn")
-  btn.addEventListener("click", () => {
-    alert("Sign Up")
-  return btn
-})
-
-// submitBtn.classList.add("active");
-// submitBtn.style.backgroundColor = "#8e44ad";
-
-
-
-let warningTimeout;
-const warningBox = document.createElement("div");
-warningBox.className = "warning";
-
-form.addEventListener("submit", validate)
-form.addEventListener("click", displayResult)
-
-
-
-
-// ----------------Functions----------------------------
-
-function validate(evt) {
-  evt.preventDefault ()
-  const nameVal = validateName()
-  if (nameVal === false) {
-    return false
+    div.classList.add("title")
+    return div
   }
-  return true
-}
-
-const name = document.getElementById("name")
-function validateName() {
-  if (userName.value === "") {
-    displayWarning("Please provide a name.")
-    userName.focus()
-    return false
+  // Not sure why my Title and Btn do not show up. 
+  // Was working at earlier stages of the project. 
+  function createBtn() {
+    const btn = document.createElement("button")
+    btn.textContent = "Click here"
+    // btn.addEventListener("click", signUp)
+    btn.classList.add("btn")
+    // btn.addEventListener("click", () => {
+    //   alert("Sign Up")
+    return btn
   }
-  return userName.value
-}
 
-// Struggled to get this fucntion to work. Was trying to 
-// make it so they could only use lowercase letters in the name section.
+  function validate(evt) {
+    const nameVal = validateName();
+    if (nameVal === false) {
+      evt.returnValue = false;
+      return false;
+    }
 
-// function checkName(evt) {
-//   evt.preventDefault();
-//   const charCode = evt.charCode;
-//   console.log(charCode)
-//   if (charCode !== 0) {
-//     if (charCode < 97 || charCode > 122) {   
-//       displayWarning("Please use lowercase letters only.");
-//     }
-//   }
-// }
+    const emailVal = validateEmail();
+    if (emailVal === false) {
+      evt.returnValue = false;
+      return false;
+    }
 
-function displayWarning (msg) {
- warningBox.textContent = msg
- setTimeout(() => {
-  warningBox.textContent = ""
- }, 2000)
- }
+    alert(`Name: ${nameVal}
+      Email: ${emailVal}`)
 
- function displayResult () {
-  const selectedContinent = continents.value
-  const name = userName.value
-  const result = `Selected continent ${selectedContinent}.`
-  resultContinent.textContent = result
- }
+    return true;
+  }
 
- console.log(form.parentNode)
- console.log(form.childNode)
+  function validateName() {
+    if (userName.value === "") {
+      displayWarning("Please provide a name.")
+      userName.focus()
+      return false
+    }
+    return userName.value
+  }
 
-})
+  // Struggled to get this fucntion to work. Was trying to 
+  // make it so they could only use lowercase letters in the name section.
+
+  function checkName(evt) {
+    evt.preventDefault();
+    const charCode = evt.charCode;
+    console.log(charCode)
+    if (charCode !== 0) {
+      if (charCode < 97 || charCode > 122) {
+        displayWarning("Please use lowercase letters only.");
+      }
+    }
+  }
+
+  function displayResult() {
+    const selectedContinent = continents.value
+    const name = userName.value
+    const result = `Selected continent ${selectedContinent}.`
+    const resultContinent = document.getElementById("result-container")
+    resultContinent.textContent = result
+  }
+
+  function validateEmail() {
+    let emailVal = email.value;
+
+    if (emailVal === "") {
+      alert("Please provide an email.");
+      email.focus();
+      return false;
+    }
+
+    const atpos = emailVal.indexOf("@");
+    const dotpos = emailVal.lastIndexOf(".");
+
+    if (atpos < 1) {
+      alert("Your email must include an @ symbol which must not be at the beginning of the email.");
+      email.focus();
+      return false;
+    }
+
+    if (dotpos - atpos < 2) {
+      alert("Invalid structure: @.\nYou must include a domain name after the @ symbol.");
+      email.focus();
+      return false;
+    }
+    return emailVal;
+
+    function createWarningBox() {
+      const warningBox = document.createElement("div")
+      warningBox.className = "warning"
+      return warningBox
+    }
+
+    let warningTimeout;
+    function displayWarning(msg) {
+      const warningBox = document.querySelector(".warning")
+      warningBox.textContent = msg
+      setTimeout(() => {
+        warningBox.textContent = ""
+      }, 2000)
+    }
+
+    // ----------------------------------------------------
+
+    form.style.border = "1px solid black"
+    form.style.padding = "10px"
+
+    console.log(window.innerWidth)
+    console.log(window.innerHeight)
+
+
+    // Was trying to edit the page with both CSS and JS.
+    // Ran into issues with one or the other working but not both.
+
+    console.log(form.parentNode)
+    console.log(form.childNodes)
+  }})
